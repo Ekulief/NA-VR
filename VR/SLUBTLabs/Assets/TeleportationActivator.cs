3f17a000504890d2cb9b7f142dd293035d689b12
LIEFLANDER LUKE <2241741@slu.edu.ph>
2026-03-30 16:26:27 +0800

    VR Basic controls
    Created a place to test the VR controls

--- VR/SLUBTLabs/Assets/TeleportationActivator.cs
+++ VR/SLUBTLabs/Assets/TeleportationActivator.cs
@@ -4,26 +4,43 @@ using UnityEngine.XR.Interaction.Toolkit.Interactors;
 public class TeleportationActivator : MonoBehaviour
 {
     public XRRayInteractor teleportInteractor;
+    public XRRayInteractor rayInteractor;
     public InputActionProperty teleportActivatorAction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public void DisableTeleportRay()
+    {
+        teleportInteractor.gameObject.SetActive(false);
+    }
     void Start()
     {
         teleportInteractor.gameObject.SetActive(false);
 
         teleportActivatorAction.action.performed += Action_performed;
+        rayInteractor.uiHoverEntered.AddListener(x => DisableTeleportRay());
     }
 
     private void Action_performed(InputAction.CallbackContext obj)
     {
+        if (rayInteractor && rayInteractor.IsOverUIGameObject())
+        {
+            
+                return;
+         
+           
+        }
         teleportInteractor.gameObject.SetActive(true);
     }
-
     // Update is called once per frame
     void Update()
-    {
-        if(teleportActivatorAction.action.WasReleasedThisFrame())
+                {
+        if (teleportActivatorAction.action.WasReleasedThisFrame())
         {
-                       teleportInteractor.gameObject.SetActive(false);
+            teleportInteractor.gameObject.SetActive(false);
         }
-    }
+                }
+        
+   
+
+
 }
